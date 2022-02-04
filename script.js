86439262cb330a563aa59238484f6aa58d3aa6cf
Nicole J. C. Lemes <nicolejdecarvalho@gmail.com>
2022-02-04 20:09:48 -0300

    cartas aparecendo embaralhadas e com flip

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let cartas = prompt("Com quantas cartas deseja jogar? Escolha um número par entre 4 e 14");
+let qtdCartas = []
 
 function quantidadeCartas() {
     while ((cartas % 2 !== 0) || (4 > cartas) || (cartas > 14) || (cartas === 0)) {
@@ -7,3 +8,38 @@ function quantidadeCartas() {
 }
 
 quantidadeCartas();
+
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+function aparecerCartas() {
+    for(let i = 0; i < cartas; i++) {
+        qtdCartas.push(`arquivos/gif${i}.gif`);
+        qtdCartas.sort(comparador); 
+    }
+
+    for (let j = 0; j < cartas; j++) {
+        const adicionar = document.querySelector("main");
+        adicionar.innerHTML = adicionar.innerHTML + `
+        <div class="total-card" onclick="girarCarta(this)">
+            <div class="front card">
+                <img src="arquivos/front.png" alt="papagaio">
+            </div>
+            <div class="back card">
+                <img src="${qtdCartas[j]}" alt="gif papagaio">
+            </div>
+        </div>
+        `
+    }
+}
+
+aparecerCartas();
+
+function girarCarta(cardEscolhido) {
+    cardEscolhido.classList.toggle("flip");
+}
+
+
+
